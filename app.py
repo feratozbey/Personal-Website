63dc62ff10a1c5703c46fd6abf525d1e615941c8
Ferat <feratozbey@gmail.com>
2022-10-21 14:32:36 +1100

    Version 1.3.3

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from csv import reader
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open('personalwebsite.csv', newline='', encoding="ISO-8859-1") as csv_file:
+        csv_data = reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('index.html', projects=list_of_rows)
 
 
 if __name__ == '__main__':
